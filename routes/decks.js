f143c2be8d1d9b15f17c2e980a7ca56395e4cf9e
Michael Yeager <m.yeager001@gmail.com>
2016-03-21 13:08:38 -0600

    deck routes

--- routes/decks.js
+++ routes/decks.js
@@ -4,7 +4,7 @@ var knex = require('../db/knex');
 var models = require('../modelsAndCollections');
 var Bookshelf = require('bookshelf')(knex);
 
-router.post('/creatDeck', function(req, res, next) {
+router.post('/createDeck', function(req, res, next) {
   var deck = {
     name: req.body.name,
     user_id: req.body.user_id,
@@ -12,6 +12,14 @@ router.post('/creatDeck', function(req, res, next) {
   knex('decks').insert(deck, 'id').then(function(id){
     deck.id = id[0];
     res.status(200).json({error: false, data: deck});
+  }).catch(function(error){
+    res.status(500).json({error: true, data: error});
+  });
+});
+
+router.delete('/:deckid', function(req, res, next) {
+  knex('decks').delete().where('id', req.params.deckid).then(function(){
+    res.status(204).json({eror: false, data:'success'});
   }).catch(function(error){
     res.status(500).json({error: true, data: error});
   });
