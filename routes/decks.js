7e21f74f8d01c7799d165e5c203f6f63154c11f3
Michael Yeager <m.yeager001@gmail.com>
2016-03-21 15:00:30 -0600

    update decks

--- routes/decks.js
+++ routes/decks.js
@@ -8,7 +8,7 @@ router.post('/createdeck', function(req, res, next) {
   var deck = {
     name: req.body.name,
     user_id: req.body.user_id,
-  }
+  };
   knex('decks').insert(deck, 'id').then(function(id){
     deck.id = id[0];
     res.status(200).json({error: false, data: deck});
@@ -23,6 +23,31 @@ router.delete('/:deckid', function(req, res, next) {
   }).catch(function(error){
     res.status(500).json({error: true, data: error});
   });
+});
+
+router.update('questions/:deckid', function(req, res, next) {
+  if(req.decoded.id == req.body.hostid){
+    var questionsInsert = []
+    req.body.questions.forEach(function(question){
+      var insert = {
+        question_id: question.id,
+        deck_id: req.body.id,
+      }
+      questionsInsert.push(insert);
+    })
+    knex('deck_questions').delete().where('deck_id', req.params.deckid).then(function(){
+      knex('deck_questions').insert(questionsInsert, 'id').where('deck_id', req.params.deckid).then(function(id){
+        console.log(id);
+        res.status(204).json({eror: false, data:'success'});
+      }).catch(function(error){
+        res.status(500).json({error: true, data: error});
+    }).catch(function(error){
+      res.status(500).json({error: true, data: error});
+    });
+  });
+  }else{
+    res.status(403).json({error: true, data: 'incorrect user_id'});
+  }
 });
 
 module.exports = router;
