37efb5d2a69bace632202936f30d73f3fd5de674
Michael Yeager <m.yeager001@gmail.com>
2016-03-22 11:03:34 -0600

    update username route

--- routes/users.js
+++ routes/users.js
@@ -31,7 +31,8 @@ router.get('/', function(req, res, next) {
 router.patch('/username', function(req, res, next){
   console.log('in this route');
   console.log(req.decoded)
-  knex('users').where('id', req.decoded.id).update({username: req.body.username}).then(function(update){
+  console.log(req)
+  knex('users').where('id', req.decoded.id).update({profile_name: req.body.username}).then(function(update){
     res.status(200).json({error: false, data: users});
   }).catch(function(error){
     res.status(500).json({error: true, data: error});
